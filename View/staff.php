ea4f3b5f860e816e25eec68161f24814e27dda5c
Rifat <mahfuzrifat10@gmail.com>
2021-04-13 12:26:39 +0600

    fixed a link

--- View/staff.php
+++ View/staff.php
@@ -10,7 +10,7 @@
 
 <body>
     <?php include 'publicHeader.php'; ?>
-    <h3 align="center">Welcome to our staff's side of the website! if you're a staff here you're in the right place. Please login if you're an existing employee or create a staff account on this if you want to become one. If you are neither a staff nor want to become one, please consider going to our <a href="index.php">Home Page</a> instead. Thank you! </h3>
+    <h3 align="center">Welcome to our staff's side of the website! if you're a staff here you're in the right place. Please login if you're an existing employee or create a staff account on this if you want to become one. If you are neither a staff nor want to become one, please consider going to our <a href="../index.php">Home Page</a> instead. Thank you! </h3>
     <fieldset>
         <center>
             <form>
