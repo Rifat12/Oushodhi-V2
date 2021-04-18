8881b8ea786e09c5f113c7a313237a135b573a8d
Rifat <mahfuzrifat10@gmail.com>
2021-04-18 23:32:36 +0600

    Order database implemented

--- View/orders.php
+++ View/orders.php
@@ -18,62 +18,8 @@ if ($_SESSION['flag'] != true) {
 <body>
     <?php include 'internalheader.php'; ?>
     <br>
-
-    <div class="wrapper">
-        <details>
-            <summary>
-                Pending Orders
-            </summary>
-
-            <table border="1">
-                <thead>
-                    <tr>
-                        <th>Order ID</th>
-                        <th>Customer ID</th>
-                        <th>Amount Worth</th>
-                        <th>Order Time</th>
-                        <th>Delivery Type</th>
-                        <th>Prescription Provided</th>
-                        <th> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</th>
-                        <th>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>2343244</td>
-                        <td>2344432</td>
-                        <td>5000</td>
-                        <td>05:00</td>
-                        <td>Express</td>
-                        <td>Yes</td>
-                        <td><a href="">Accept</a></td>
-                        <td><a href="">Reject</a></td>
-                    </tr>
-                    <tr>
-                        <td>234355</td>
-                        <td>234455</td>
-                        <td>4000</td>
-                        <td>21:00</td>
-                        <td>Express</td>
-                        <td>Yes</td>
-                        <td><a href="">Accept</a></td>
-                        <td><a href="">Reject</a></td>
-                    </tr>
-                    <tr>
-                        <td>34443</td>
-                        <td>43344</td>
-                        <td>1444</td>
-                        <td>15:00</td>
-                        <td>Regular</td>
-                        <td>No</td>
-                        <td><a href="">Accept</a></td>
-                        <td><a href="">Reject</a></td>
-                    </tr>
-                </tbody>
-            </table>
-
-        </details>
-    </div>
+    <?php require("../Controller/orderProcess.php"); ?>
+    <br>
 
     <div class="wrapper">
         <details>
